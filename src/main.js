949f573d322ede6f3466d4aa0072570b1e2e4bf4
Billie Bell <billie.bell@net-up.de>
2022-02-19 04:50:05 +0100

    inital commit

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import { router } from "./router";
 import { store } from "./store";
 import App from "./App.vue";
 
+import './assets/scss/styles.scss';
+
 const app = createApp(App);
 
 app.use(createStore(store));
