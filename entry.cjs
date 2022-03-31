b71c09d9445493cdb21c0e3cf1bd3af7e6e5352e
Billie Bell <billie.bell@net-up.de>
2022-03-31 19:09:19 +0200

    entry hack

--- entry.cjs
+++ entry.cjs
@@ -1,5 +1,5 @@
 // hack for passenger to work with ESM modules
 async function myFunc() {
-    const { itsMine } = await import(",/.output/server/index.mjs");
+    const { itsMine } = await import("./.output/server/index.mjs");
 }
 myFunc()
